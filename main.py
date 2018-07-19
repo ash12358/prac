d66ff9c1ac74b8097c94705a49e4e1c8812bc88c
ash12358 <1628415175@qq.com>
2018-07-19 15:59:33 +0800

    添加了README.md 添加了utils.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import utils
 
 
 if __name__ == '__main__':
     username = 'haha'
-    print(username)
+    # print(username)
+    print(utils.get_username(username))
     pass
---
+++ utils.py
@@ -0,0 +1,4 @@
+
+
+def get_username(username):
+    return username
